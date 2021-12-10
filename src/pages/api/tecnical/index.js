7843e48cbee2a825bdd730d703677cb445d207e4
joeypinto <fansubsbrasil@gmail.com>
2021-12-10 13:37:23 -0300

    j

--- src/pages/api/tecnical/index.js
+++ src/pages/api/tecnical/index.js
@@ -1,10 +1,11 @@
 
-const { db } = await connectToDatabase()
-const collection = db.collection('TECNICALS_API')
-const collectionCalendar = db.collection('CALENDAR_DAYS')
 
-export default function (req, res) {
+export default async function (req, res) {
   const { method } = req
+  const { db } = await connectToDatabase()
+  const collection = db.collection('TECNICALS_API')
+  const collectionCalendar = db.collection('CALENDAR_DAYS')
+
   switch (method) {
     case 'GET':
       try {
@@ -37,31 +38,32 @@ export default function (req, res) {
       res.status(400).send({ error: 'O servidor não entendeu a requisição pois está com uma sintaxe inválida.' })
       break
   }
+  const getCalendar = async (id) => {
+    var respCalendar = []
+    var calendar = await collectionCalendar
+      .find({
+        $and: [
+          { ID_TECHNICAL: id },
+          { 'CALENDAR.date': { $gte: getSunday(0) } }
+        ]
+      })
+      .toArray()
+    if (calendar.length < 1) {
+      calendar.push({ ID_TECHNICAL: id, CALENDAR: CALENDAR_DAYS })
+    }
+    //Essa funcao vai ajustar o  json para o padrao exigido no frontend
+    for (const [idx, elCalendar] of calendar.entries()) {
+      elCalendar.CALENDAR.forEach(element => {
+       // retorna apenas se existir vagas naquela data.
+        if(element.scheduledPeriods[0].amount + element.scheduledPeriods[1].amount + element.scheduledPeriods[2].amount !== 0){
+        respCalendar.push({periods:element.scheduledPeriods, dateCalender:element.date})
+       }
+
+     });
+    }
+    return respCalendar
+  }
 }
 
 
-const getCalendar = async (id) => {
-  var respCalendar = []
-  var calendar = await collectionCalendar
-    .find({
-      $and: [
-        { ID_TECHNICAL: id },
-        { 'CALENDAR.date': { $gte: getSunday(0) } }
-      ]
-    })
-    .toArray()
-  if (calendar.length < 1) {
-    calendar.push({ ID_TECHNICAL: id, CALENDAR: CALENDAR_DAYS })
-  }
-  //Essa funcao vai ajustar o  json para o padrao exigido no frontend
-  for (const [idx, elCalendar] of calendar.entries()) {
-    elCalendar.CALENDAR.forEach(element => {
-     // retorna apenas se existir vagas naquela data.
-      if(element.scheduledPeriods[0].amount + element.scheduledPeriods[1].amount + element.scheduledPeriods[2].amount !== 0){
-      respCalendar.push({periods:element.scheduledPeriods, dateCalender:element.date})
-     }
 
-   });
-  }
-  return respCalendar
-}
